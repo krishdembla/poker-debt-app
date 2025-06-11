fc61e0ed3672e06993464db8e2f2736dcd814795
Krish Dembla <kdembla@iu.edu>
2025-06-11 15:01:38 +0530

    Backend: Create games, add players and buy ins and cashout

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,79 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
+const {v4: uuidv4} = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 5050;
+
+app.use(cors()); //enable CORS for all routes
+app.use(express.json());
+
+const games = {}; //in memory storage
+
+
+
+//adding route to creae a new game
+app.post('/api/game', (req, res) => {
+  const gameId = uuidv4(); //generate id
+  games[gameId] = { players: {} }; //initialize game with empty players object
+  res.status(201).json({ gameId }); //respond with gameId to frontend
+})
+
+
+
+//adding players and buy ins
+app.post('/api/game/:id/player', (req,res) => {
+  const {id} = req.params; //getting game id
+  const {name, buyIn} = req.body;
+
+  //validate input
+  if (!games[id]) {
+    return res.status(404).json({ error: 'Game not found' });
+  }
+  //validate player name and buyIn
+  if (!name || !buyIn || isNaN(buyIn) || buyIn <= 0) {
+    return res.status(400).json({ error: 'Invalid player or buy-in' });
+  }
+  const game = games[id];
+  
+  //if player does not exist, create a new player, else add buyIn to existing player
+  if (!game.players[name]) {
+    game.players[name] = { buyIns: [buyIn], cashOut: null};
+  } else{
+    game.players[name].buyIns.push(buyIn);
+  }
+
+  res.status(200).json({ message: `Buy-in added for ${name}`, player: game.players[name] });
+})
+
+
+
+
+//adding cash out for players
+app.post('/api/game/:id/cashout', (req, res) => {
+  const {id} = req.params;
+  const {name, cashOut} = req.body;
+
+  //validate input
+  if (!games[id]) {
+    return res.status(404).json({ error: 'Game not found' });
+  }
+  if (!name || cashOut == null || isNaN(cashOut)) {
+    return res.status(400).json({ error: 'Invalid player or cash out amount' });
+  }
+
+  const player = games[id].players[name];
+  if(!player) {
+    return res.status(404).json({ error: 'Player not found' });
+  }
+
+  player.cashOut = cashOut; //set cash out amount for player
+  res.status(200).json({ message: `Cash out recorded for ${name}`, player });
+});
+
+
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -15,8 +86,6 @@ app.use((req, res, next) => {
 
   next();
 });
-
-app.use(express.json());
 
 app.get('/api/ping', (req, res) => {
   res.json({ message: 'pong' });
