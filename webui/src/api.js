86f1b264905655bd04f62943c76d800513a38371
Krish Dembla <kdembla@iu.edu>
2025-07-10 16:54:55 +0530

    Deploying to Vercel

--- webui/src/api.js
+++ webui/src/api.js
@@ -6,7 +6,7 @@ export const setToken = (t) => localStorage.setItem(TOKEN_KEY, t);
 export const clearToken = () => localStorage.removeItem(TOKEN_KEY);
 const getToken = () => localStorage.getItem(TOKEN_KEY);
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5051/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5050/api";
 const api = axios.create({
   baseURL: API_URL, 
 });
